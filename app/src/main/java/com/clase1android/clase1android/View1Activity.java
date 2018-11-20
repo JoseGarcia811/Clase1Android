7071307477eda1ed7834eb0de10be8b43866b081
INTECADM\jose.garcia <jose.garcia>
2018-11-20 16:45:54 -0400

    clase del ListView del 20/11/18

--- app/src/main/java/com/clase1android/clase1android/View1Activity.java
+++ app/src/main/java/com/clase1android/clase1android/View1Activity.java
@@ -1,13 +1,49 @@
 package com.clase1android.clase1android;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class View1Activity extends AppCompatActivity {
+
+    NotificationCompat.Builder notification;
+    String NOTIFICATION_CHANNEL_ID = "channel_id_01";
+    private static final int id =3434;
+    private Button btn_notification;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vista1);
 
+        btn_notification = (Button) findViewById(R.id.notificacion);
+        notification = new NotificationCompat.Builder(this,NOTIFICATION_CHANNEL_ID);
+        notification.setAutoCancel(true);
+
+        btn_notification.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                notification.setSmallIcon(R.mipmap.ic_launcher);
+                notification.setTicker("Nueva Notificaciones");
+                notification.setPriority(Notification.PRIORITY_HIGH);
+                notification.setWhen(System.currentTimeMillis());
+                notification.setContentText("Diplomado Android - Practica II");
+                notification.setContentTitle("Practica 2 Android");
+
+                Intent intent = new Intent(View1Activity.this,View1Activity.class);
+                PendingIntent pendingIntent = PendingIntent.getActivity(View1Activity.this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+
+
+                notification.setContentIntent(pendingIntent);
+                NotificationManager notificactionManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+                notificactionManager.notify(id,notification.build());
+            }
+        });
     }
 }
--- app/src/main/java/com/clase1android/clase1android/View4Activity.java
+++ app/src/main/java/com/clase1android/clase1android/View4Activity.java
@@ -8,6 +8,6 @@ public class View4Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.vista4);
+        setContentView(R.layout.view4);
     }
 }
